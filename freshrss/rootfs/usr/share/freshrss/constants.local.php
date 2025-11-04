5440207a1c789c5dce7d6d30ec715c84d6180552
Carlos Sanmartín Bustos <carsanbu@gmail.com>
2025-11-04 12:38:50 +0100

    Move data directory to config

--- freshrss/rootfs/usr/share/freshrss/constants.local.php
+++ freshrss/rootfs/usr/share/freshrss/constants.local.php
@@ -1,7 +1,7 @@
 <?php
 
 //This directory must be writable
-defined('DATA_PATH') or define('DATA_PATH', '/data/freshrss');
+defined('DATA_PATH') or define('DATA_PATH', '/config/data');
 
 // Custom extensions directory
 defined('THIRDPARTY_EXTENSIONS_PATH') or define('THIRDPARTY_EXTENSIONS_PATH', '/config/extensions');
